b8aea62cb0b2fd3fd6e86074138dd2fcd0c78ddd
Caleb Sacks <16855387+clabe45@users.noreply.github.com>
2022-03-26 17:56:55 +0000

    test: unit test app

---
+++ tests/unit/test_app.py
@@ -0,0 +1,25 @@
+import pytest
+
+from backup.app import App
+
+
+class TestApp:
+	def test_parse_with_one_root_calls_root_parse(self, mocker):
+		Root_parse = mocker.patch('backup.root.Root.parse')
+
+		raw_root = {
+			'name': 'x',
+			'src': 'bar',
+			'dest': 'foo',
+			'match': '''+ hello
+			- world
+			'''
+		}
+
+		raw_app = {
+			'roots': [raw_root]
+		}
+
+		App.parse(raw_app)
+
+		Root_parse.assert_called_once_with(raw_root)
