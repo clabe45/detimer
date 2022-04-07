f5bbdb4a3cbf05302e6218d2ee089c685dce2193
Caleb Sacks <16855387+clabe45@users.noreply.github.com>
2022-04-07 22:39:39 +0000

    feat: create config if it doesn't exit

--- detimer/config.py
+++ detimer/config.py
@@ -7,9 +7,26 @@ from yaml import Loader, load
 from detimer.constants import APP_NAME
 
 
-PATH = os.path.join(get_app_dir(APP_NAME), 'config.yml')
+_script_dir = os.path.dirname(__file__)
+
+CONFIG_DIR = get_app_dir(APP_NAME)
+CONFIG_FILE = os.path.join(CONFIG_DIR, 'config.yml')
+DEFAULT_FILE = os.path.join(_script_dir, '..', 'data', 'default-config.yml')
 
 
 def load_config():
-	with open(PATH, 'r') as f:
+	if not os.path.exists(CONFIG_DIR):
+		os.mkdir(CONFIG_DIR)
+	elif not os.path.isdir(CONFIG_DIR):
+		raise Exception(f'{CONFIG_DIR} is not a directory')
+
+	if not os.path.exists(CONFIG_FILE):
+		with open(CONFIG_FILE, 'w+') as real, open(DEFAULT_FILE, 'r') as default:
+			default_config = default.read()
+			real.write(default_config)
+
+	elif not (os.path.isfile(CONFIG_FILE) or os.path.islink(CONFIG_FILE)):
+		raise Exception(f'{CONFIG_FILE} must be a file or a symlink')
+
+	with open(CONFIG_FILE, 'r') as f:
 		return load(f.read(), Loader=Loader)
