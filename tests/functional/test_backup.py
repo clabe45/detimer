c46b8fe70f2f005cd9a8ebeef440028585b2be22
Caleb Sacks <16855387+clabe45@users.noreply.github.com>
2023-01-15 22:00:12 -0500

    test: convert backup unit test to functional test

--- tests/functional/test_backup.py
+++ tests/functional/test_backup.py
@@ -10,6 +10,12 @@ def test_backup_with_default_config_file_outputs_done(run, no_config):
     assert result.output == "Done\n"
 
 
+def test_backup_with_no_roots_prints_help_message_and_aborts(run, no_config):
+    result = run("backup")
+
+    assert "Usage: cli backup [OPTIONS] [ROOTS]..." in result.output
+
+
 def test_backup_with_force_and_all_confirms(run, no_config):
     result = run("backup", "-a", "-f")
 
--- tests/unit/test_commands.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-from detimer.commands import backup
-
-
-def test_backup_with_no_roots_prints_help_message_and_aborts():
-    with pytest.raises(SystemExit):
-        backup()
